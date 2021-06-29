9a0afb5f976a8ae2290251b3be89d6756d97d9b4
Harry Frahn <harrisonfrahn@gmail.com>
2021-06-29 14:52:31 -0700

    update website title

--- week1/scripts/script.js
+++ week1/scripts/script.js
@@ -1,5 +1,6 @@
 const map = L.map('map').setView([34.0709, -118.444], 8);
 
+
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -14,7 +15,7 @@ fetch("camping.geojson")
             // the leaflet method for adding a geojson
             L.geoJSON(data, {
                 pointToLayer: function (feature, latlng) {
-                    return L.circleMarker(latlng, {color: "red"});
+                    return L.circleMarker(latlng, {color: "blue"});
                 }
             }).bindPopup(function (layer) {
                 return layer.feature.properties.name;
